7775950d9e751bd3fdc7adb653e41c12f2951891
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-04-23 11:55:36 +0100

    add new artist and album, not working

--- record_store/controllers/album_controller.rb
+++ record_store/controllers/album_controller.rb
@@ -10,11 +10,18 @@ erb(:"album/index")
 end
 
 get "/album/new" do
-@artists = Artist.all
+@artists = Artist.all()
 erb(:new)
 end
+
 
 get "/album/:id" do
   @album = Album.find(params['id'].to_i)
   erb(:"album/show")
+end
+
+post "/album" do
+new_album = Album.new(params)
+new_album.save()
+redirect to "/"
 end
--- record_store/controllers/artist_controller.rb
+++ record_store/controllers/artist_controller.rb
@@ -8,7 +8,19 @@ get '/artist' do
   erb(:"artist/index")
 end
 
+get '/artist/new' do
+@artists = Artist.all()
+  erb(:"artist/new")
+end
+
 get '/artist/:id' do
   @artist = Artist.find(params['id'].to_i)
   erb(:"artist/show")
+end
+
+
+post "/artist" do
+new_artist = Artist.new(params)
+new_artist.save()
+redirect to "/"
 end
